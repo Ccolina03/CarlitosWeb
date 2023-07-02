ab6d9f93a34b57fcfb4056edb64a7c555b79cfc0
Ccolina03 <colinaperu2003@gmail.com>
2023-07-01 20:46:17 -0600

    Cards next to job icon positioning

--- src/components/experiences/Experiences.tsx
+++ src/components/experiences/Experiences.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Heading from '../Heading';
 import UAlberta from '../../gallery/icon-ualberta.svg'
-
+import './Experiences.css'
 const Experiences = () => {
     return (
         <div className='flex flex-col items-center gap-16 '>
@@ -9,13 +9,20 @@ const Experiences = () => {
             <div className='experience-container flex justify-center'>
                 <div className='timeline w-28 bg-gray-800 flex flex-col items-center'>
                     <div className='line1 w-0.5 bg-gray-100 h-36'></div>
-                    <div className='circle1 rounded-full border-2 border-white h-28 w-full'></div>
+                    <div className=' circle1 relative overflow-visible flex justify-center items-center rounded-full border-2 border-white h-28 w-full hover:bg-green '>
+                         <img src={UAlberta} alt='UAlberta icon'></img>
+                         <div className='job1 absolute z-30 bg-green rounded w-[50vw]'>
+                                Job
+                        </div>
+                    </div>
                     <div className='line1 w-0.5 bg-gray-100 h-96'></div>
-                    <div className='circle1 rounded-full border-2 border-white h-28 w-full hover:bg-green'>
+                    <div className='circle1 flex items-center justify-center rounded-full border-2 border-white h-28 w-full hover:bg-green'>
                         <img src={UAlberta} alt='UAlberta icon'></img>
                     </div>
                     <div className='line1 w-0.5 bg-gray-100 h-96'></div>
-                    <div className='circle1 rounded-full border-2 border-white h-28 w-full'></div>
+                    <div className='circle1 flex justify-center items-center rounded-full border-2 border-white h-28 w-full hover:bg-green'>
+                        <img src={UAlberta} alt='UAlberta icon'></img>
+                    </div>
                 </div> 
             </div>
         </div>
