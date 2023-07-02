5d88ebdcf86174997bb0a51ba73af7bcee0082a4
Ccolina03 <colinaperu2003@gmail.com>
2023-07-02 16:21:30 -0600

    Last update before introduing Next.js 13

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,14 @@
 import ProjectItem from './ProjectItem'
-
+import Heading from './Heading';
 const Projects = () => {
     return (
+    <div className='projects-container gap-16 flex flex-col'>
+        <Heading className="text-4xl self-center text-white" size='h1'>My Projects</Heading>
         <div className='grid sm:grid-cols-1 md:grid-cols-2'>
             <ProjectItem/>
             <ProjectItem/>
             <ProjectItem/>
+      </div>
       </div>
     )
 }
